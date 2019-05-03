54aeb6a5f84578db9831b31651470247c6dbc701
Katrin Shibakova <katrina.gubanova@gmail.com>
2019-05-03 14:42:37 +0300

    added component of the Logo

---
+++ src/components/logo/logo.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import PropTypes from 'prop-types';
+
+const Logo = (props) => {
+  const {className} = props;
+
+  return (
+    <div className="logo">
+      <a className={`logo__link ${className}`}>
+        <span className="logo__letter logo__letter--1">W</span>
+        <span className="logo__letter logo__letter--2">T</span>
+        <span className="logo__letter logo__letter--3">W</span>
+      </a>
+    </div>
+  );
+};
+
+Logo.propTypes = {
+  className: PropTypes.string
+};
+
+Logo.defaultProps = {
+  className: ``
+};
+
+export default Logo;
